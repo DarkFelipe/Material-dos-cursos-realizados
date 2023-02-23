b68014834b1dd87969ef78660883fc5ec0b661bb
DarkFelipe <105126346+DarkFelipe@users.noreply.github.com>
2023-02-23 15:35:14 -0300

    Add files via upload

---
+++ Curso_POO_IFSP/Projetos_Netbeans/A02Ex02/src/a02ex02/A02Ex02.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package a02ex02;
+
+/**
+ *
+ * @author PC
+ */
+public class A02Ex02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("primeiro argumento: " +args[0]);
+        System.out.println(args[0]);
+        System.out.println("segundo argumento: " +args[1]);
+        
+    }
+}
